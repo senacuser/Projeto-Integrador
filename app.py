9f82f35c3737cb0e21126fcdb918d234c338bef1
Clodomilson <clodomilsonanjos.eng@outlook.com>
2023-12-22 14:07:46 -0300

    Correção de push

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/telaprincipal')
+def telaprincipal():
+    return render_template('telaprincipal.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+@app.route('/calendario')
+def calendario():
+    return render_template('calend.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
